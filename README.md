7bcf482ea45a2235bf0caffdfe93c17ef3294043
Kris Breuker <kris@b-lex.nl>
2016-09-20 17:10:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Homebridge GPIO
+
+Supports triggering General Purpose Input Output (GPIO) pins on platforms supporting the [onoff](https://www.npmjs.com/package/onoff) package. It is based on [homebridge-gpio](https://www.npmjs.com/package/homebridge-gpio), but using the onoff package instead of pi-gpio to be able to listen for changes in pin values.
+
+## Requirements
+-  [Homebridge](https://github.com/nfarina/homebridge) - _HomeKit support for the impatient_
+-  [gpio-admin](https://www.npmjs.com/package/onoff) - _Reading and writing GPIO pins and listening for changes_
+
+## Installation
+1.  Install Homebridge using `npm install -g homebridge`
+2.  Install this plugin `npm install -g homebridge-gpio-onoff`
+3.  Update your configuration file
+
+## Configuration
+Example `config.json`, creating a switch accessory that the user can switch on and off, setting the GPIO pin value to '1' or '0', respectively:
+
+```json
+{
+  "accessories": [
+    {
+      "accessory": "GPIO-OnOff",
+      "name": "GPIO-7",
+      "pin": 7,
+      "direction": "out"
+    }
+  ]
+}
+```
+
+`accessory` should be set to `GPIO-OnOff` for this plugin.
+`name` is the accessory's name, which is shown in HomeKit.
+`pin` is the physical GPIO pin number.
+`direction` can be set to either `in` to read from the GPIO pin or `out` to write to the pin. If set to `in`, the `edge` property can be set to `rising`, `falling` or `both` to have a changing GPIO pin value trigger an accessory value change (it is set to `none` by default).
+
+By default, a simple 'Switch' accessory is created, which will be set to 'On' or 'Off' by a change in the input value. It is possible to change the service type and even the `true` and `false` values. For example, to have it act like a light sensor, you can specify:
+
+```json
+{
+  "accessories": [
+    {
+      "accessory": "GPIO-OnOff",
+      "name": "GPIO-7",
+      "pin": 7,
+      "direction": "in",
+      "edge": "both",
+      "activeLow": true,
+      "hapService": "LightSensor",
+      "hapCharacteristic": "CurrentAmbientLightLevel",
+      "trueValue": 100000,
+      "falseValue": 0.0001
+    }
+  ]
+}
+```
+
+`activeLow` will treat '0' as the `true` value and '1' as the `false` value.
+`hapService` can be set to one of the defined services in [HomeKitTypes.js](https://github.com/KhaosT/HAP-NodeJS/blob/master/lib/gen/HomeKitTypes.js) and `hapCharacteristic` to one of the characteristics defined for the service.
+If the characteristic values are not boolean, you can specify the values for `true` and `false` by setting `trueValue` and `falseValue`, respectively.
---
+++ index.js
@@ -0,0 +1,131 @@
+"use strict";
+
+var Gpio = require("onoff").Gpio;
+var Service, Characteristic;
+
+module.exports = function(homebridge)
+{
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  // Register this accessory as 'GPIO-OnOff'
+  homebridge.registerAccessory("homebridge-gpio-onoff", "GPIO-OnOff", GPIOOnOffAccessory);
+};
+
+function GPIOOnOffAccessory(log, config)
+{
+  this.log = log;
+
+  // Read configuration
+  this.name = config.name;
+  let pin = config.pin;
+  let direction = config.direction;
+  let edge = config.edge;
+  let activeLow = !!config.activeLow;
+
+  let hapService = config.hapService;
+  this.hapCharacteristic = config.hapCharacteristic;
+  this.trueValue = config.trueValue;
+  this.falseValue = config.falseValue;
+
+  // Check configuration
+  if (!pin)
+    throw new Error("You must provide a config value for pin.");
+  if ([ "in", "out" ].indexOf(direction) < 0)
+    direction = "in";
+  if ([ "none", "rising", "falling", "both" ].indexOf(edge) < 0)
+    edge = "none";
+
+  // If the specified service or characteristic is unknown, fall back to generic Switch
+  if (!(hapService in Service) || !(this.hapCharacteristic in Characteristic))
+    hapService = "Switch";
+  // If the service is a Switch, use the On characteristic (will also be triggered when service was unknown)
+  if (hapService == "Switch")
+  {
+    this.hapCharacteristic = "On";
+    this.trueValue = true;
+    this.falseValue = false;
+  }
+
+  // Initialize the GPIO object
+  this.sensor = new Gpio(pin, direction, edge, { activeLow: activeLow });
+
+  // Initialize the switch service
+  this.service = new Service[hapService](this.name);
+  if (direction == "in")
+  {
+    this.log("Initializing 'in' GPIO accessory on pin " + pin);
+    this.service
+        .getCharacteristic(Characteristic[this.hapCharacteristic])
+        .on("get", this.readGPIO.bind(this));
+
+    if (edge != "none")
+      this.sensor.watch(this.onGPIOChange.bind(this));
+  }
+  else if (direction == "out")
+  {
+    this.log("Initializing 'out' GPIO accessory on pin " + pin);
+    this.service
+        .getCharacteristic(Characteristic[this.hapCharacteristic])
+        .on("set", this.writeGPIO.bind(this));
+  }
+}
+
+GPIOOnOffAccessory.prototype.getServices = function()
+{
+  return [this.service];
+};
+
+GPIOOnOffAccessory.prototype.gpioToCharacteristic = function(value)
+{
+  return value == 1 ? this.trueValue : this.falseValue;
+};
+
+GPIOOnOffAccessory.prototype.characteristicToGPIO = function(value)
+{
+  return value == this.trueValue;
+};
+
+GPIOOnOffAccessory.prototype.readGPIO = function(callback)
+{
+  this.log("Reading GPIO value");
+  this.sensor.read((err, value) =>
+  {
+    if (err)
+      callback(err);
+    else
+    {
+      let charValue = this.gpioToCharacteristic(value);
+      this.log(`Read GPIO value ${value} as ${charValue}`);
+      callback(null, charValue);
+    }
+  });
+};
+
+GPIOOnOffAccessory.prototype.writeGPIO = function(value, callback)
+{
+  let gpioValue = this.characteristicToGPIO(value);
+  this.log(`Writing Characteristic value ${value} as ${gpioValue}`);
+  this.sensor.write(gpioValue, err =>
+  {
+    if (err)
+      callback(err);
+    else
+    {
+      this.log("Wrote GPIO value");
+      callback();
+    }
+  });
+};
+
+GPIOOnOffAccessory.prototype.onGPIOChange = function(err, value)
+{
+  if (!err)
+  {
+    let charValue = this.gpioToCharacteristic(value);
+    this.log(`Received GPIO value ${value} as ${charValue}`);
+    this.service
+        .getCharacteristic(Characteristic[this.hapCharacteristic])
+        .setValue(charValue);
+  }
+};
